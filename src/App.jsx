00894da8a58802a8afacb6abfe47464c707cbf5c
Munkh-Iveel <iveelv968@gmail.com>
2025-08-20 10:32:18 +0800

    approved index.html and app.jsx and main.jsx and added errorboundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import HomePage from './pages/HomePage/HomePage'
-import CoursesPage from './pages/CoursesPage/CoursesPage'
-import ProductsPage from './pages/ProductsPage/ProductsPage'
-import LiveLessonsPage from './pages/LiveLessonsPage/LiveLessonsPage'
+const CoursesPage = lazy(() => import("./pages/CoursesPage/CoursesPage"));
+const ProductsPage = lazy(() => import("./pages/ProductsPage/ProductsPage"));
+const LiveLessonsPage = lazy(() => import("./pages/LiveLessonsPage/LiveLessonsPage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
+
+import ErrorBoundary from './pages/ErrorBoundary'
 
 import NotificationPage from './pages/NotificationPage'
 import CheckoutPage from './pages/CheckoutPage' // Adjusted path
 import CartPage from './pages/CartPage'
-import AboutPage from './pages/AboutPage'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -17,10 +19,16 @@ function App() {
 
   return (
     <Router>
+      <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
+            <Route path="/error-boundary" element={
+              <ErrorBoundary>
+                <div>Test Error Boundary</div>
+              </ErrorBoundary>
+            } />
             <Route path="/courses" element={<CoursesPage />} />
             <Route path="/products" element={<ProductsPage />} />
             <Route path="/live-lessons" element={<LiveLessonsPage />} />
@@ -29,7 +37,8 @@ function App() {
             <Route path="/notification" element={<NotificationPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
           </Routes>
-        </Router>
+      </Suspense>
+    </Router>
   )
 }
 
